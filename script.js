c0205160b13a5ffa94183f3f8b54873056f0e6c7
Lawrence <lawrenceomozojie@gmail.com>
2025-06-16 01:14:28 +0100

    - Add projects, about me and contact sections. - Change cta-btn background color

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const hamMenu = document.querySelector(".ham-menu");
-
 const offScreenMenu = document.querySelector(".nav-links");
 
 hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    hamMenu.classList.remove("active");
+    offScreenMenu.classList.remove("active");
+  });
 });
