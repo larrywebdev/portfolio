b93445853fab8ebf138fcf101428616c69f58a98
Lawrence <lawrenceomozojie@gmail.com>
2025-06-16 11:38:01 +0100

    Enhance navigation and footer styles; add overflow handling for links

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const hamMenu = document.querySelector(".ham-menu");
 const offScreenMenu = document.querySelector(".nav-links");
-
 hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
+  updateBodyScroll();
 });
 
 document.querySelectorAll(".nav-links a").forEach((link) => {
   link.addEventListener("click", () => {
     hamMenu.classList.remove("active");
     offScreenMenu.classList.remove("active");
+    updateBodyScroll();
   });
 });
+
+function updateBodyScroll() {
+  if (hamMenu.classList.contains("active")) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "";
+  }
+}
